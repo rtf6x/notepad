b28b3594ce78a006501f5a589778769f5244e43b
Alex Trapholov <trapholov@gmail.com>
2016-07-17 11:15:29 +0300

    fix: logout

--- client/app/logout/route.js
+++ client/app/logout/route.js
@@ -3,6 +3,7 @@
 
     angular.module('app.logout').config(function($routeProvider){
         $routeProvider.when('/logout', {
+            template: '',
             controller: 'logout'
         });
     });
